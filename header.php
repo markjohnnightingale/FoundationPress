5fd70c6b113aa682ca355bdd59993dc4eaa64637
Mark Nightingale <mark.john.nightingale@gmail.com>
2014-05-25 16:04:32 +0200

    Add static CSS

--- header.php
+++ header.php
@@ -21,7 +21,9 @@
       echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
     } ?></title>
     
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/foundation.css" />
+	
     
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
